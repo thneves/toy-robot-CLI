e596d76a13921a4e53b89360c9af9be2e9fbbdd0
thneves <tsneves11@gmail.com>
2022-01-09 04:57:44 -0300

    Implement modules and play class

--- bin/main.rb
+++ bin/main.rb
@@ -1,37 +1,45 @@
 require_relative '../lib/robot'
+require_relative '../lib/play'
 
 program_on = true
 
 while program_on
-  puts '-----------------------------------------'
-  puts '                 TOY ROBOT               '
-  puts '-----------------------------------------'
-  puts 'INSTRUCTIONS:'
-  puts 'these are the instructions to the toy robot'
-  puts 'INITIAL COMMANDS:'
-  puts 'PLACE'
 
-  # instruções
-  # place -> posição x, posição y
-  # facing -> north, south, west, east
-  # move -> move one position into facing direction
-  # rotate -> left and right
-  # report -> show current location and where the robot is facing
-  robot = Robot.new
-  robot.place
-  puts robot.x_position.to_s
-  puts robot.y_position.to_s
-  robot.move(robot.f_orientation)
-  puts robot.x_position.to_s
-  puts robot.y_position.to_s
-  robot.rotate
-  robot.move(robot.f_orientation)
-  puts robot.x_position.to_s
-  puts robot.y_position.to_s
-  puts robot.report
-  program_on = false
+  toy_robot = ToyRobot.new
+  toy_robot.intro
+
+  while toy_robot.current_command != 'exit'
+    puts "Digit a command: \n"
+    toy_robot.current_command = gets.chomp.downcase
+    case toy_robot.current_command
+    when 'place'
+      robot = Robot.new
+      robot.place
+    when 'move'
+      robot.move(robot.f_orientation)
+    when 'rotate'
+      robot.rotate
+    when 'report'
+      robot.report
+    when 'exit'
+      puts "Thanks for playing around :)"
+      program_on = false
+    else
+      puts "Digit a valid command: Place, Move, Rotate, Report or Exit"
+    end
+  end
+  # robot = Robot.new
+  # robot.place
+  # puts robot.x_position.to_s
+  # puts robot.y_position.to_s
+  # robot.move(robot.f_orientation)
+  # puts robot.x_position.to_s
+  # puts robot.y_position.to_s
+  # robot.rotate
+  # robot.move(robot.f_orientation)
+  # puts robot.x_position.to_s
+  # puts robot.y_position.to_s
+  # puts robot.report
+  # program_on = false
 end
 
-# 0..4
-# 0..4
-#
